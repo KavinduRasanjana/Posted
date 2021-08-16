7c513d7000bdd449310e44966521c4c1b22c9232
KavinduRasanjana <freedomkavindu@gmail.com>
2021-08-16 20:31:19 +0530

    initial commit

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/register.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form action="">
+            <input type="text" placeholder="Enter Your Name">
+            <input type="text" placeholder="Enter Your Email">
+            <input type="text" placeholder="Contact Number">
+            <input type="password" placeholder="Enter Your Password">
+            <button type="submit"> Register</button>
+            <button type="reset" id="clear">Clear</button>
+        </form>
+    </div>
+</body>
+
+</html>
